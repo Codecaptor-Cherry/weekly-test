45ff875e64deb4c15950b6a4ab39f7f48f49a9fa
모나카롱 <80571085+monacaron@users.noreply.github.com>
2024-02-24 11:43:53 +0900

    Create BJ_2531_회전초밥.java

---
+++ week53/HeeJoo/BJ_2531_회전초밥.java
@@ -0,0 +1,60 @@
+package saturday.sat240224;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/*
+ * 회전 벨트 위 여러 가지 종류의 초밥(번호 중복 가능)
+ * 행사 1. 벨트의 임의의 한 위치부터 k개의 접시를 연속해서 먹을 경우 할인된 정액 가격으로 제공
+ * 행사 2. 각 고객에게 초밥의 종류 하나가 쓰인 쿠폰 발행 후, 행사 1에 참가할 경우 쿠폰에 적혀진 종류의 초밥 하나 무료 제공
+ * 쿠폰에 적혀진 초밥이 벨트 위에 없을 경우, 새로 만들어 제공
+ * 벨트 상태, 메뉴에 있는 초밥의 가짓수, 연속해서 먹는 개수, 쿠폰 번호가 주어질 때, 손님이 먹을 수 있는 초밥 가짓수의 최댓값 구하기
+ */
+public class BJ_2531_회전초밥 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]); // 벨트 길이 N
+        int d = Integer.parseInt(input[1]); // 초밥 가짓수 d
+        int k = Integer.parseInt(input[2]); // 연속 접시 개수 k
+        int c = Integer.parseInt(input[3]); // 쿠폰 번호 c
+
+        int[] belt = new int[N];
+        for(int i = 0; i < N; i++) {
+            belt[i] = Integer.parseInt(br.readLine());
+        }
+
+        int ans = 0;
+        int tmp = 0;
+        int[] menu = new int[d + 1];
+        for(int i = 0; i < k; i++) {
+            if(menu[belt[i]] == 0) {
+                tmp++;
+            }
+            menu[belt[i]]++;
+        }
+
+        ans = menu[c] == 0 ? tmp + 1 : tmp;
+        for(int i = 1; i < N; i++) {
+            int left = belt[i - 1];
+            int right = belt[i + k - 1 < N ? i + k - 1 : i + k - 1 - N];
+
+            if(menu[left] == 1) {
+                tmp--;
+            }
+            menu[left]--;
+
+            if(menu[right] == 0) {
+                tmp++;
+            }
+            menu[right]++;
+
+            ans = Math.max(ans, menu[c] == 0 ? tmp + 1 : tmp);
+        }
+
+        System.out.println(ans);
+    }
+}
