f1f68f60e52f92cfd017a5b74a8c487632761cd3
subinan <060657@naver.com>
2025-05-10 11:53:32 +0900

    Create N번째 큰 수.java

---
+++ week93/Subin/N번째 큰 수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n * n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                nums[i * n + j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        Arrays.sort(nums);
+        System.out.println(nums[n * n - n]);
+    }
+}
